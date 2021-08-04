499d7cafabac2c2cb2498f3fd71d25e52ac33a36
chitkala2802 <chitkala2802@gmail.com>
2021-08-04 17:04:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-yqty5x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-yqty5x)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+...
+import {WeatherComponent} from './weather/weather.component';
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
+...
+@NgModule({
+    declarations:[
+      ...
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(allAppRoutes)
+    ]
+    ...
+})
+...
